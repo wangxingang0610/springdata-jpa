0c4fe1174c6c4ab66e4052959e341a896f2712d2
wangxingang4 <wangxingang@jd.com>
2018-12-28 11:37:49 +0800

    springdata---Repository 接口

--- src/main/java/com/springdata/domain/Address.java
+++ src/main/java/com/springdata/domain/Address.java
@@ -10,6 +10,26 @@ public class Address {
 
     private String addressName;
 
+    private String province;
+
+    private String city;
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     @GeneratedValue
     @Id
     public Integer getId() {
